73aae75df2e5cb9e484a391be9e187ad25f2b787
AD0791 <alexdisla1991@gmail.com>
2021-02-03 16:48:44 -0500

    bull

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,16 @@
-import 'package:flutter/foundation.dart';
+//import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:device_preview/device_preview.dart';
+//import 'package:device_preview/device_preview.dart';
 
 import './quiz.dart';
 import './result.dart';
 
 void main() => runApp(
-      DevicePreview(
-        enabled: !kReleaseMode,
-        builder: (context) => MyApp(), // Wrap your app
-      ),
-    );
+    // DevicePreview(
+    //   enabled: !kReleaseMode,
+    //   builder: (context) => MyApp(), // Wrap your app
+    // ),
+    MyApp());
 
 class MyApp extends StatefulWidget {
   @override
@@ -68,8 +68,8 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        locale: DevicePreview.locale(context), // Add the locale here
-        builder: DevicePreview.appBuilder, // Add the builder here
+        //locale: DevicePreview.locale(context), // Add the locale here
+        //builder: DevicePreview.appBuilder, // Add the builder here
         home: Scaffold(
             appBar: AppBar(
               title: Text("My first App"),
